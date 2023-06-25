259f3bf125a7f47f2d24256e46d274280f5bb494
Gaëtan Pierre-Justin <gaetan.pierrejustin@vista.com>
2023-06-25 22:27:03 +0200

    rename

--- add_zsh_fzf_dbt_profile_safely.sh
+++ add_zsh_fzf_dbt_profile_safely.sh
@@ -4,8 +4,8 @@ NL='
 '
 
 # technique heavily inspired by : https://github.com/paul-hammant/switchjdk/blob/master/add_switchjdk_to_bash_profile_safely.sh
-bashProfile=$(cat ~/.zprofile | sed '/fzf_dbt.sh/d')
-echo "${bashProfile}${NL}${NL}# Leave this fzf_dbt.sh incantation as a single line, so that homebrew upgrades are smooth${NL}if [ -f $(brew --prefix)/etc/fzf_dbt.sh ]; then source $(brew --prefix)/etc/fzf_dbt.sh; fi${NL}" > ~/.bash_profile
+zProfile=$(cat ~/.zprofile | sed '/fzf_dbt.sh/d')
+echo "${zProfile}${NL}${NL}# Leave this fzf_dbt.sh incantation as a single line, so that homebrew upgrades are smooth${NL}if [ -f $(brew --prefix)/etc/fzf_dbt.sh ]; then source $(brew --prefix)/etc/fzf_dbt.sh; fi${NL}" > ~/.zprofile
 
 echo "*** Did the above fail with permissions errors? ***"
 echo "If yes, you will need to do this after homebrew finishes (once off):"
